17523c87496a14c18c74838b5af42c0a58f532ae
Farya <farya.hsn@gmail.com>
2021-09-27 15:44:59 +0200

    test for mutliple deposits

--- bank_test.rb
+++ bank_test.rb
@@ -6,15 +6,24 @@ class Account
     @name = name
     @balance = 0
     @date = Time.now.strftime("%d/%m/%Y")
+    @numb_of_calc = 0
+    @numb_of_deposits = 0
+    @deposit_amount = 0
   end
 
-  def calculate_balance 
-    @balance += @deposit_amount.to_f
-    @balance -= @withdraw_amount.to_f
+  def balance
+    if @numb_of_calc >= @numb_of_deposits
+      @balance
+    else
+      @numb_of_calc += 1
+      calculate_balance
+      @balance
+    end
   end
 
   def deposit(deposit_amount)
-    @deposit_amount = deposit_amount
+    @numb_of_deposits += 1
+    @deposit_amount += deposit_amount
   end 
 
   def withdraw(withdraw_amount)
@@ -24,6 +33,13 @@ class Account
   def transaction
     calculate_balance
     "#{@date} || #{@withdraw_amount} || #{@deposit_amount} || #{@balance}"
+  end
+
+  private
+
+  def calculate_balance 
+    @balance += @deposit_amount.to_f
+    @balance -= @withdraw_amount.to_f
   end
 
 end
--- spec/bank_test_spec.rb
+++ spec/bank_test_spec.rb
@@ -23,14 +23,12 @@ describe Account do
 
   it "returns updated balance after deposit" do
     account.deposit(150.00)
-    account.calculate_balance
     expect(account.balance).to eq 150.00
   end
 
   it "returns updated balance after withdrawal" do
     account.deposit(400.00)
     account.withdraw(150.00)
-    account.calculate_balance
     expect(account.balance).to eq 250.00
   end
 
@@ -42,9 +40,20 @@ describe Account do
 
   it "should only return balance without any calculations" do
     account.deposit(300)
-    account.calculate_balance
     account.balance
     expect(account.balance).to eq 300
+  end
 
+  it "should allow for multiple deposits" do
+    account.deposit(300)
+    account.deposit(300)
+    expect(account.balance).to eq 600
+  end
+
+  it "should allow for multiple withdrawals" do
+    account.deposit(1000)
+    account.withdrawal(300)
+    account.withdrawal(300)
+    expect(account.balance).to eq 400
   end
 end
