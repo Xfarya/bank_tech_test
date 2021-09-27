0162f72f177b55b57f4fce16b22f157f1add79af
Farya <farya.hsn@gmail.com>
2021-09-27 14:24:22 +0200

    updated tests and added one for transaction test

--- bank_test.rb
+++ bank_test.rb
@@ -1,23 +1,33 @@
 class Account
 
+  attr_accessor :deposit_amount, :withdraw_amount
+  
   def initialize(name)
     @name = name
+    @balance = 0
+    @date = Time.now.strftime("%d/%m/%Y")
   end
 
   def balance 
-    400.00
+    @balance += @deposit_amount.to_f
+    @balance -= @withdraw_amount.to_f
   end
 
   def deposit(deposit_amount)
-    deposit_amount = 150.00
+    @deposit_amount = deposit_amount
   end 
 
   def withdraw(withdraw_amount)
-    withdraw_amount = 150.00
+    @withdraw_amount = withdraw_amount
   end 
 
   def date
     "27/09/2021"
+  end
+
+  def transaction
+    balance
+    "#{@date} || #{@withdraw_amount} || #{@deposit_amount} || #{@balance}"
   end
 
 end
--- spec/bank_test_spec.rb
+++ spec/bank_test_spec.rb
@@ -5,7 +5,7 @@ describe Account do
   let(:account) { described_class.new("John") }
 
   it "returns the users account balance" do
-    expect(account.balance).to eq 400.00
+    expect(account.balance).to eq 0
   end
 
   it "returns the users deposit amount" do
@@ -19,5 +19,22 @@ describe Account do
   it "returns the date today" do
     date = Time.now.strftime("%d/%m/%Y")
     expect(account.date).to eq date
+  end
+
+  it "returns updated balance after deposit" do
+    account.deposit(150.00)
+    expect(account.balance).to eq 150.00
+  end
+
+  it "returns updated balance after withdrawal" do
+    account.deposit(400.00)
+    account.withdraw(150.00)
+    expect(account.balance).to eq 250.00
+  end
+
+  it "prints a line of required fields" do
+    account.deposit(300)
+    account.withdraw(150)
+    expect(account.transaction).to eq "27/09/2021 || 150 || 300 || 150.0"
   end
 end
