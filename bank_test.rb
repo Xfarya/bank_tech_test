b3ce7c6f1085893f77f2808cee023d926950da4b
Farya <farya.hsn@gmail.com>
2021-09-28 18:08:37 +0200

    some refactoring and all tests passing

--- bank_test.rb
+++ bank_test.rb
@@ -2,82 +2,91 @@ class Account
 
   attr_accessor :debit, :credit, :date, :balance, :print
   
+  # creates new account, with name and statement data all set
   def initialize(new_account_name, balance = 0, date = Time.now.strftime("%d/%m/%Y"), deposit_amount = 0, withdraw_amount = 0)
     @name = new_account_name
-    puts "Hi #{@name}, your account was created!"
     @date = date
     @credit = withdraw_amount
     @debit = deposit_amount
     @balance = balance
-    @deposited = false
-    @withdrawn = false
+    puts "Hi #{@name}, your account was created!"
+  # array to hold array of statement values
     @print = []
-
-    @numb_of_calc = 0
-   
-    @numb_of_transactions = 0
-    
-    
-  end
-
-  def calculate
-    if @numb_of_calc >= @numb_of_transactions
-      @balance
-    else
-      @numb_of_calc += 1
-      calculate_balance
-      @balance
-    end
-
     @deposited = false
     @withdrawn = false
+  # this is for updating the statment seperately everytime a new transaction is made
+    @numb_of_transactions = 0
+    @numb_of_requests = 0
   end
 
   def deposit(deposit_amount)
-    @deposited = true
-    @numb_of_transactions += 1 
+    # bringing value into instance variable
     @debit = deposit_amount
+    # registering that amount has been deposited
+    @deposited = true
+    # keeping count of number of requests
+    @numb_of_requests += 1 
+    # calculate the sums
     calculate
+    # value that is returned by calling this method
     @debit
   end 
 
   def withdraw(withdraw_amount)
-    @withdrawn = true
-    @numb_of_transactions += 1
+    # bringing value into instance variable
     @credit = withdraw_amount
+    # registering that amount has been credited
+    @withdrawn = true
+    # keeping count of number of requests
+    @numb_of_requests += 1
+    # calculate the sums
     calculate
+    # value that is returned by calling this method
     @credit
   end 
 
-  def transaction
-    # @print << Receipt.new(@date, @withdraw_amount, @deposit_amount, @balance)
-    @print << [ @date, @credit, @debit, @balance ]
-    clear_transaction
-  end
-
   def print_receipt
-    puts @transaction = ["date  ||  credit  ||  debit  ||  balance"]
-    @table = @print.each do |d, c, deb, b|
+    # will add values to statement array, and also clear deposit/withdraw pots
+    commit_transaction
+    # will output a table in the console
+    puts @transaction_table = ["date  ||  credit  ||  debit  ||  balance"]
+    @table = @print.reverse.each do |d, c, deb, b|
       puts ["#{d} || #{c} ||  #{deb}  ||  #{b}"].join('\n')
     end
   end
 
   private
 
+  def calculate
+    if @numb_of_transactions >= @numb_of_requests
+      @balance
+    else
+      @numb_of_transactions += 1
+      calculate_balance
+      # @balance
+    end
+    @deposited = false
+    @withdrawn = false
+  end
+
   def calculate_balance 
     if @deposited == true
       @balance += @debit.to_f
-    elsif @withdrawn == true
+    elsif @withdrawn == true && @balance > 0
       @balance -= @credit.to_f
+    elsif @balance < 0
+      puts "Sorry, you do not have enough balance to withdraw at this time!"
     end
   end
 
-  def clear_transaction
+  def commit_transaction
+    @print << [ @date, @credit, @debit, @balance ]
+    clear_transaction_data
+  end
+
+  def clear_transaction_data
     @debit = 0
     @credit = 0
   end
-
-  
-
 
 end
--- spec/bank_test_spec.rb
+++ spec/bank_test_spec.rb
@@ -4,8 +4,15 @@ require 'time'
 describe Account do
   let(:account) { described_class.new("John") }
 
-  it "returns the users initial account balance" do
+  it "returns the users initial account balances" do
     expect(account.balance).to eq 0
+    expect(account.credit).to eq 0
+    expect(account.debit).to eq 0
+  end
+
+  it "returns the date today" do
+    date = Time.now.strftime("%d/%m/%Y")
+    expect(account.date).to eq date
   end
 
   it "returns the users deposit amount" do
@@ -14,11 +21,6 @@ describe Account do
 
   it "returns the users withdrawal amount" do
     expect(account.withdraw(150)).to eq 150.00
-  end
-
-  it "returns the date today" do
-    date = Time.now.strftime("%d/%m/%Y")
-    expect(account.date).to eq date
   end
 
   it "returns updated balance after deposit" do
@@ -37,8 +39,8 @@ describe Account do
   it "prints a line of required fields" do
     account.deposit(300)
     account.withdraw(150)
-    account.transaction
-    expect(account.print).to eq "[Date: '28/09/2021' || Withdraw: '150' || Deposit: '300' || Balance '150.0']"
+    account.print_receipt
+    expect(account.print).to eq [["28/09/2021", 150, 300, 150.0]]
   end
 
   it "should only return balance without any calculations" do
@@ -59,7 +61,7 @@ describe Account do
     expect(account.balance).to eq 400
   end
 
-  it "should allow for multiple withdrawals" do
+  it "should allow to check balance despite multiple requests" do
     account.deposit(1000)
     account.withdraw(300)
     account.balance
@@ -69,26 +71,26 @@ describe Account do
 
   it "should return a receipt for multiple transactions" do
     account.deposit(1000)
-    account.transaction
+    account.print_receipt
     account.deposit(1000)
-    account.transaction
+    account.print_receipt
     # p account.transaction
-    expect(account.print).to eq "[Date: '28/09/2021' || Withdraw: '0' || Deposit: '1000' || Balance '1000.0'][Date: '28/09/2021' || Withdraw: '0' || Deposit: '1000' || Balance '2000.0']"
+    expect(account.print).to eq [["28/09/2021", 0, 1000, 1000.0], ["28/09/2021", 0, 1000, 2000.0]]
   end
 
   it "should return transactions with latest first" do
     account.deposit(1000)
-    account.transaction
+    account.print_receipt
     account.deposit(1000)
-    account.transaction
-    expect(account.print_receipt).to eq "[Date: '28/09/2021' || Withdraw: '0' || Deposit: '1000' || Balance '1000.0'][Date: '28/09/2021' || Withdraw: '0' || Deposit: '1000' || Balance '2000.0']"
+    account.print_receipt
+    expect(account.print_receipt).to eq [["28/09/2021", 0, 0, 2000.0], ["28/09/2021", 0, 1000, 2000.0], ["28/09/2021", 0, 1000, 1000.0]]
   end
 
 
   it "should empty deposit and withdrawal sums after transaction is printed" do
     account.deposit(1000)
     account.withdraw(1000)
-    account.transaction
+    account.print_receipt
     expect(account.debit).to eq 0
   end
 end
